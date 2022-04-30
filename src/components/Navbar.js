7b77dd7194459b658e5130fc7f2c7ba3845fcb8c
ssourbh000 <86834692+ssourbh000@users.noreply.github.com>
2022-04-30 10:21:25 +0530

    commit router

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+
+export const Navbar = () => {
+  return (
+    <nav>
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='/about'>About</NavLink>
+    </nav>
+  )
+}
